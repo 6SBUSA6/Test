105e7c9b28c3f5813fef3f703f35a7833911fe00
gyw666 <1813434069@qq.com>
2021-12-22 17:02:18 +0800

    oneBranch添加了age属性

--- FruitManager/src/com/gyw/bean/Student.java
+++ FruitManager/src/com/gyw/bean/Student.java
@@ -7,4 +7,22 @@ package com.gyw.bean;
  */
 public class Student {
     private int sid;
+    //oneBranch添加了一个age
+    private int age;
+
+    public int getSid() {
+        return sid;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
